6d94440cddaf8a21e35b4d3385ee7fe48a1d9824
Kriegslustig <github@ls7.ch>
2014-12-05 23:08:06 +0100

    converting tabs to spaces

--- functions.php
+++ functions.php
@@ -6,10 +6,10 @@
  */
 add_action('wp_enqueue_scripts', 'bin_wp_enqueue_styles');
 function bin_wp_enqueue_styles () {
-	wp_register_style('reset', get_bloginfo('template_directory').'/css/meyers-reset.css');
-	wp_enqueue_style('reset');
-	wp_register_style('styles', get_bloginfo('template_directory').'/css/styles.css');
-	wp_enqueue_style('styles');
+  wp_register_style('reset', get_bloginfo('template_directory').'/css/meyers-reset.css');
+  wp_enqueue_style('reset');
+  wp_register_style('styles', get_bloginfo('template_directory').'/css/styles.css');
+  wp_enqueue_style('styles');
 }
 
 /*
@@ -18,8 +18,8 @@ function bin_wp_enqueue_styles () {
  */
 add_action('wp_enqueue_scripts', 'bin_wp_enqueue_scripts');
 function bin_wp_enqueue_scripts () {
-	wp_enqueue_script( 'prefixfree', get_bloginfo('template_directory').'/js/prefixfree.min.js', array(), false, true);
-	wp_enqueue_script( 'gallerizer', get_bloginfo('template_directory').'/js/gallerizer.js', array(), false, true);
+  wp_enqueue_script( 'prefixfree', get_bloginfo('template_directory').'/js/prefixfree.min.js', array(), false, true);
+  wp_enqueue_script( 'gallerizer', get_bloginfo('template_directory').'/js/gallerizer.js', array(), false, true);
 }
 
 /*
@@ -28,28 +28,28 @@ function bin_wp_enqueue_scripts () {
  */
 add_action('customize_register', 'customize_allemaedchenintheworld');
 function customize_allemaedchenintheworld ($wp_customize) {
-	/*
-	 * Adding a section for all the customizations to put in
-	*/
-	$wp_customize->add_section('main', [
-		'title' => 'Main',
-		'priority' => '1'
-	]);
-	/*
-	 * Adding a setting for the main_text, it is read and interpreted as a post inside the main element on the front page.
-	*/
-	$wp_customize->add_setting('main_text', [
-		'default' => ''
-	]);
-	/*
-	 * Adding the actual textarea
-	*/
-	$wp_customize->add_control('main_text_control', [
-		'label' => 'Main content on frontpage',
-		'settings' => 'main_text',
-		'section' => 'main',
-		'type' => 'textarea'
-	]);
+  /*
+   * Adding a section for all the customizations to put in
+  */
+  $wp_customize->add_section('main', [
+    'title' => 'Main',
+    'priority' => '1'
+  ]);
+  /*
+   * Adding a setting for the main_text, it is read and interpreted as a post inside the main element on the front page.
+  */
+  $wp_customize->add_setting('main_text', [
+    'default' => ''
+  ]);
+  /*
+   * Adding the actual textarea
+  */
+  $wp_customize->add_control('main_text_control', [
+    'label' => 'Main content on frontpage',
+    'settings' => 'main_text',
+    'section' => 'main',
+    'type' => 'textarea'
+  ]);
 }
 
 /*
@@ -60,18 +60,18 @@ function customize_allemaedchenintheworld ($wp_customize) {
  */
 add_filter('get_image_tag', 'add_wrapper_full_size');
 function add_wrapper_full_size ($html) {
-	$size = preg_match_all('/class\=\"[\w\s]*size\-([\w-_]*)/', $html, $matches);
-	$size = $matches[1][0];
-	$wrapper = <<<HTML
+  $size = preg_match_all('/class\=\"[\w\s]*size\-([\w-_]*)/', $html, $matches);
+  $size = $matches[1][0];
+  $wrapper = <<<HTML
 <div class="image_wrapper--size--full">%s</div>
 HTML;
-	if($size === 'full') {
-		return sprintf($wrapper, $html);
-	}
-	return $html;
+  if($size === 'full') {
+    return sprintf($wrapper, $html);
+  }
+  return $html;
 }
 
 /*
- *	This is the cleanest way to remove the default styles from galleries
+ *  This is the cleanest way to remove the default styles from galleries
  */
 add_theme_support('html5', array('gallery'));
