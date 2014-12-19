e9e276a87183570304b6eb86bbbea988e2eb767a
Kriegslustig <github@ls7.ch>
2014-12-19 18:33:46 +0100

    Adding Focus and extending Article and config

--- js/articleSwitcher.js
+++ js/articleSwitcher.js
@@ -13,8 +13,14 @@
  * ArticleSwitcher = {}
  * - config = {}
  *   - article = {}
- *     - openClass = {}
+ *     - class
+ *     - openClass
  *     - focusClass
+ *     - offsetTop
+ *     - titleClass
+ *   - focus
+ *     - class
+ *     - animationTime
  *
  * Article = {}
  * - _node = DOMNode
@@ -25,12 +31,16 @@
  * - open()
  * - focus()
  * - unFocus()
+ * - updatePos()
+ * - getHeight()
+ * - appendChild()
  *
  * Focus = {}
  *   This should be the line which jumps from title to title
  * Should have
+ * - flyingThingi = DOMNode
+ * - _position = integer
  * - articleIndex = [Article, Article, ...]
- * - position = integer
  * - goto() uses scrollTo, sets position
  *
  * Controll
@@ -44,22 +54,44 @@
 articleSwitcher = {
   config: {
     article: {
-      openClass: 'article--open';
-      focusClass: 'article--focus';
-    }
+      class: 'article',
+      openClass: 'article--open',
+      focusClass: 'article--focus',
+      offsetTop: '20',
+      titleClass: 'article__title',
+    },
+    focus: {
+      class = 'focusLine',
+      animationTime: 200,
+    },
   }
 };
 
 articleSwitcher.createArticle = (function (node) {
   var _node = node,
-  state = {
+  _state = {
     focus: false,
     open: false
   },
-  titlePosition,
-  size = {
+  _titlePosition {
+    x,
+    y
+  },
+  _size = {
     height: _node.height,
     width: _node.width
+  };
+  function _getOffset () {
+    var pos = {
+      x: _node.offsetLeft,
+      y: _node.offsetTop
+    },
+    child = _node;
+    while(child = child.offsetParent) {
+      pos.x += child.offsetLeft;
+      pos.y += child.offsetTop;
+    }
+    return pos;
   };
   return {
     open: function () {
@@ -77,5 +109,38 @@ articleSwitcher.createArticle = (function (node) {
     unFocus: function () {
       _node.className = _node.className.replace('' + this.config.focusClass, '');
     },
+
+    updatePos: function () {
+     _titlePosition = _getOffset();
+    },
+
+    getHeight: function () {
+      return _size.height;
+    }
+
+    appendElement: function (element) {
+      _node.appendChild(element);
+    }
+  }
+});
+
+articleSwitcher.createFocus = (function () {
+  var _position = 0,
+  _flyingThingi = document.createElement('div'),
+
+  _flyingThingi.className = this.config.focus.class;
+  document.body.appendChild(flyingThingi);
+  return {
+    articleIndex,
+    goto: function (index) {
+      _flyingThingi.remove();
+      articleIndex[index].appendElement(_flyingThingi);
+      var moveBy = articleIndex[_position].getHeight() + this.config.article.offsetTop;
+      _flyingThingi.css.transform = 'translateY: '+ moveBy + 'px';
+      setTimeout(function () {
+        _flyingThingi.remove();
+        articleIndex[index].appendElement(_flyingThingi);
+      }, this.config.focus.animationTime);
+    }
   }
 });
