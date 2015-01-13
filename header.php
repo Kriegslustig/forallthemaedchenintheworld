5f78546ca6dbf85de495b64e8b657510cb48799f
Kriegslustig <github@ls7.ch>
2015-01-13 11:28:31 +0100

    Integrating Typekit

--- header.php
+++ header.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title><?= bloginfo('blog_title'); ?></title>
-	<meta charset="<?php bloginfo( 'charset' ); ?>" />
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<link rel="shortcut icon" sizes="16x16 24x24 32x32 48x48 64x64" href="<?= site_url() ?>/favicon.ico">
+  <title><?= bloginfo('blog_title'); ?></title>
+  <meta charset="<?php bloginfo( 'charset' ); ?>" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="shortcut icon" sizes="16x16 24x24 32x32 48x48 64x64" href="<?= site_url() ?>/favicon.ico">
 <?php
-		wp_head();
+  wp_head();
 ?>
+  <script src="//use.typekit.net/snk3tib.js"></script>
+  <script>try{Typekit.load();}catch(e){}</script>
 </head>
 <body>
